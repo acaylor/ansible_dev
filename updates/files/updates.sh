81ba4ecccb459391ecfb0648ee1a170966e18a28
Emrakool <ajcaylor@gmail.com>
2021-02-20 12:52:56 -0500

    yum and apt updates

--- updates/files/updates.sh
+++ updates/files/updates.sh
@@ -4,4 +4,10 @@
 echo "you need to have ansible"
 
 #Debian
-ansible raspbian -m apt -a "update_cache=yes upgrade=yes" -b -K
+ansible debian -m apt -a "update_cache=yes upgrade=yes" -u root -b -k
+
+#Centos
+ansible centos -m yum -a "name=* state=latest" -u root -b -k
+
+#reboot
+ansible all -a "reboot" -u root -b -k
